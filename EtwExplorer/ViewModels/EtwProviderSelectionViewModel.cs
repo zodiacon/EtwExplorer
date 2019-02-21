e1338ddebabf003ad49e3d7087157d6937c9444d
Pavel Yosifovich <zodiacon@live.com>
2019-02-21 20:54:58 +0200

    added toggle to show most likely manifests to exist

--- EtwExplorer/ViewModels/EtwProviderSelectionViewModel.cs
+++ EtwExplorer/ViewModels/EtwProviderSelectionViewModel.cs
@@ -26,6 +26,7 @@ namespace EtwExplorer.ViewModels {
 
 		public EtwProviderSelectionViewModel(Window dialog) : base(dialog) {
 			CollectionViewSource.GetDefaultView(Providers).Filter = null;
+			IsLikely = true;
 		}
 
 		public IEnumerable<Provider> Providers => _providers;
@@ -42,8 +43,9 @@ namespace EtwExplorer.ViewModels {
 			set {
 				if (SetProperty(ref _searchText, value)) {
 					var cvs = CollectionViewSource.GetDefaultView(Providers);
-					if (string.IsNullOrWhiteSpace(_searchText))
-						cvs.Filter = null;
+					if (string.IsNullOrWhiteSpace(_searchText)) {
+						cvs.Filter = IsLikely ? obj => (((Provider)obj).Name.IndexOf(' ') < 0) : (Predicate<object>)null;
+					}
 					else {
 						string text = _searchText.ToLower();
 						cvs.Filter = obj => {
@@ -59,6 +61,20 @@ namespace EtwExplorer.ViewModels {
 		public bool CloseCurrentManifest {
 			get => _closeCurrentManifest;
 			set => SetProperty(ref _closeCurrentManifest, value);
+		}
+
+		bool _isLikely;
+		public bool IsLikely {
+			get => _isLikely;
+			set {
+				if (SetProperty(ref _isLikely, value)) {
+					var cvs = CollectionViewSource.GetDefaultView(Providers);
+					if (!string.IsNullOrWhiteSpace(SearchText))
+						return;
+					cvs.Filter = IsLikely ? obj => (((Provider)obj).Name.IndexOf(' ') < 0) : (Predicate<object>)null;
+
+				}
+			}
 		}
 	}
 }
